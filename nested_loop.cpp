83df428f59a705b2191e7e367d112063aadc78e9
saksheegupta11 <saksheegupta520@gmail.com>
2026-05-12 00:41:10 +0530

    first commit

---
+++ nested_loop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    int i,j,n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    for(i=1; i<=10; i++){
+        for(j=1; j<=n; j++){
+            cout<<setw(5)<<i*j;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
